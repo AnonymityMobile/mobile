eaac2e18ed4b6955ca8da1ffe737e1a34965e398
mobile <101018149+AnonymityMobile@users.noreply.github.com>
2022-11-09 00:01:18 +0100

    Add files via upload

---
+++ RQ4/anything/check_path.py
@@ -0,0 +1,28 @@
+import os
+check_list = os.listdir('/mnt/irisgpfs/users/pycharm/data/anything_image_pkl')
+
+path_pre_data_anything = [
+    ['augmentation_brightness_anything_image_data.pkl', 'prediction_anything_augmentation_brightness_anything_image_data.pkl'],
+    ['augmentation_channel_shift_range_anything_image_data.pkl', 'prediction_anything_augmentation_channel_shift_range_anything_image_data.pkl'],
+    ['augmentation_featurewise_std_normalization_anything_image_data.pkl', 'prediction_anything_augmentation_featurewise_std_normalization_anything_image_data.pkl'],
+    ['augmentation_height_shift_anything_image_data.pkl', 'prediction_anything_augmentation_height_shift_anything_image_data.pkl'],
+    ['augmentation_horizontal_flip_anything_image_data.pkl', 'prediction_anything_augmentation_horizontal_flip_anything_image_data.pkl'],
+    ['augmentation_rotation_anything_image_data.pkl', 'prediction_anything_augmentation_rotation_anything_image_data.pkl'],
+    ['augmentation_shear_range_anything_image_data.pkl', 'prediction_anything_augmentation_shear_range_anything_image_data.pkl'],
+    ['augmentation_vertical_flip_anything_image_data.pkl', 'prediction_anything_augmentation_vertical_flip_anything_image_data.pkl'],
+    ['augmentation_width_shift_anything_image_data.pkl', 'prediction_anything_augmentation_width_shift_anything_image_data.pkl'],
+    ['augmentation_zca_whitening_anything_image_data.pkl', 'prediction_anything_augmentation_zca_whitening_anything_image_data.pkl'],
+    ['augmentation_zoom_anything_image_data.pkl', 'prediction_anything_augmentation_zoom_anything_image_data.pkl'],
+    ['contrast_anything_image_data.pkl', 'prediction_anything_contrast_anything_image_data.pkl'],
+    ['noise_gasuss_anything_image_data.pkl', 'prediction_anything_noise_gasuss_anything_image_data.pkl'],
+    ['noise_random_anything_image_data.pkl', 'prediction_anything_noise_random_anything_image_data.pkl'],
+    ['noise_salt_pepper_anything_image_data.pkl', 'prediction_anything_noise_salt_pepper_anything_image_data.pkl'],
+    ['anything_image_data.pkl', 'prediction_anything_anything_image_data.pkl']
+]
+
+
+for i in path_pre_data_anything:
+    if i[0] and i[1] in check_list:
+        print('yes')
+    else:
+        print('==============')
